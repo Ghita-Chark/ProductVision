5488f512ed8975126dc96d9fe0ba9759c6b7745c
ghita252555 <charkghita2@gmailcom>
2025-06-02 15:50:57 +0100

    adding admin required

--- App/controllers/auth_controller.py
+++ App/controllers/auth_controller.py
@@ -5,7 +5,6 @@ from functools import wraps
 auth = Blueprint('auth', __name__)
 
 def login_required(f):
-    """Décorateur pour restreindre l'accès aux utilisateurs connectés."""
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if 'user_id' not in session:
@@ -15,11 +14,10 @@ def login_required(f):
     return decorated_function
 
 def admin_required(f):
-    """Décorateur pour restreindre l'accès aux administrateurs."""
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if 'user_id' not in session or session.get('role') != 'admin':
-            flash("Accès refusé : Vous devez être administrateur pour accéder à cette page ❌")
+            flash("Accès refusé : Vous devez être administrateur pour accéder à cette page!!")
             return redirect(url_for('auth.login'))
         return f(*args, **kwargs)
     return decorated_function
